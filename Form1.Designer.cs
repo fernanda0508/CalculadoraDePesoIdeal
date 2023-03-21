80d78a53a220bccfe55997a2d53e17c7f3306800
Fernanda <fefecoelho0508@gmail.com>
2023-03-21 10:11:06 -0400

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace PesoIdeal
+{
+    partial class CalculoDePesoIdeal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbnMasculino = new System.Windows.Forms.RadioButton();
+            this.rbnFeminino = new System.Windows.Forms.RadioButton();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblPesoIdeal = new System.Windows.Forms.Label();
+            this.lblPesoIdeal2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbnFeminino);
+            this.groupBox1.Controls.Add(this.rbnMasculino);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 71);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sexo";
+            // 
+            // rbnMasculino
+            // 
+            this.rbnMasculino.AutoSize = true;
+            this.rbnMasculino.Location = new System.Drawing.Point(18, 26);
+            this.rbnMasculino.Name = "rbnMasculino";
+            this.rbnMasculino.Size = new System.Drawing.Size(105, 24);
+            this.rbnMasculino.TabIndex = 0;
+            this.rbnMasculino.TabStop = true;
+            this.rbnMasculino.Text = "Masculino";
+            this.rbnMasculino.UseVisualStyleBackColor = true;
+            this.rbnMasculino.CheckedChanged += new System.EventHandler(this.rbnMasculino_CheckedChanged);
+            // 
+            // rbnFeminino
+            // 
+            this.rbnFeminino.AutoSize = true;
+            this.rbnFeminino.Location = new System.Drawing.Point(130, 26);
+            this.rbnFeminino.Name = "rbnFeminino";
+            this.rbnFeminino.Size = new System.Drawing.Size(99, 24);
+            this.rbnFeminino.TabIndex = 1;
+            this.rbnFeminino.TabStop = true;
+            this.rbnFeminino.Text = "Feminino";
+            this.rbnFeminino.UseVisualStyleBackColor = true;
+            this.rbnFeminino.CheckedChanged += new System.EventHandler(this.rbnFeminino_CheckedChanged);
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(286, 13);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(55, 20);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "Altura:";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(290, 39);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(100, 26);
+            this.txtAltura.TabIndex = 2;
+            // 
+            // lblPesoIdeal
+            // 
+            this.lblPesoIdeal.AutoSize = true;
+            this.lblPesoIdeal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPesoIdeal.Location = new System.Drawing.Point(159, 113);
+            this.lblPesoIdeal.Name = "lblPesoIdeal";
+            this.lblPesoIdeal.Size = new System.Drawing.Size(94, 22);
+            this.lblPesoIdeal.TabIndex = 3;
+            this.lblPesoIdeal.Text = "Peso Ideal";
+            // 
+            // lblPesoIdeal2
+            // 
+            this.lblPesoIdeal2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblPesoIdeal2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPesoIdeal2.ForeColor = System.Drawing.Color.Red;
+            this.lblPesoIdeal2.Location = new System.Drawing.Point(150, 143);
+            this.lblPesoIdeal2.Name = "lblPesoIdeal2";
+            this.lblPesoIdeal2.Size = new System.Drawing.Size(113, 48);
+            this.lblPesoIdeal2.TabIndex = 4;
+            this.lblPesoIdeal2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CalculoDePesoIdeal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 243);
+            this.Controls.Add(this.lblPesoIdeal2);
+            this.Controls.Add(this.lblPesoIdeal);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "CalculoDePesoIdeal";
+            this.Text = "Cálculo de Peso Ideal";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbnFeminino;
+        private System.Windows.Forms.RadioButton rbnMasculino;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblPesoIdeal;
+        private System.Windows.Forms.Label lblPesoIdeal2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PesoIdeal
+{
+    public partial class CalculoDePesoIdeal : Form
+    {
+        RadioButton rbnSelecionado = null;
+
+        public CalculoDePesoIdeal()
+        {
+            InitializeComponent();
+        }
+
+        private void rbnMasculino_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rbn = (RadioButton)sender;
+            if (rbn.Checked)
+            {
+                rbnSelecionado = rbn;
+                SetPesoIdeal();
+            }
+        }
+
+        private void rbnFeminino_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rbn = (RadioButton)sender;
+            if (rbn.Checked)
+            {
+                rbnSelecionado = rbn;
+                SetPesoIdeal();
+            }
+        }
+        //comentario
+        private void SetPesoIdeal()
+        {
+            try
+            {
+                double altura = Convert.ToDouble(txtAltura.Text);
+                double pesoIdeal;
+                if (rbnSelecionado.Text.Equals("Masculino"))
+                    pesoIdeal = (72.7 * altura) - 58;
+                else
+                    pesoIdeal = (62.1 * altura) - 44.7;
+                lblPesoIdeal2.Text = pesoIdeal.ToString("N");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Selecione o sexo e informe a altura corretamente", "Atenção!",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
